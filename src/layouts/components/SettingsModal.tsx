9a0a341ac88cdc51e7854ef3e1d65ad21dcf45e6
rsl150101 <dnwnd501@naver.com>
2026-03-10 17:45:17 +0900

    refactor: 하드 코딩된 Worker URL 주소 환경 변수 적용
    - SettingModal 내 직접 작성된 URL을 VITE_WORKER_URL 환경 변수로 대체

--- src/layouts/components/SettingsModal.tsx
+++ src/layouts/components/SettingsModal.tsx
@@ -95,7 +95,7 @@ const Input = styled.input`
   }
 `;
 
-const WORKER_URL = "https://dragonsword-map-db.shrill-art-2219.workers.dev";
+const WORKER_URL = import.meta.env.VITE_WORKER_URL;
 
 export const SettingsModal = ({ onClose }: { onClose: () => void }) => {
   const { customMarkers, resetCustomMarkers, importCustomMarkers } = useMapStore();
